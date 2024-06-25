b548a7efdf2ee0213f474edb77905aeab5eecb26
David Ludwig <davidludwigii@gmail.com>
2024-06-25 23:41:59 +0000

    Add TransformerEncoderBlock and TransformerEncoder

--- src/deepdna/nn/layers.py
+++ src/deepdna/nn/layers.py
@@ -1,9 +1,10 @@
 import abc
+import copy
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Optional, Union
+from typing import cast, Sequence, Optional, Union
 
 # Multi-head Attention Mechanisms ------------------------------------------------------------------
 
@@ -19,7 +20,6 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        self.dropout = dropout
         self.bias = bias
         if head_embed_dim is None:
             assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads if head_embed_dim is not provided"
@@ -31,6 +31,7 @@ class MultiHeadAttention(nn.Module):
         self.w_key = nn.Linear(embed_dim, self.head_embed_dim * num_heads, bias=bias)
         self.w_value = nn.Linear(embed_dim, self.head_embed_dim * num_heads, bias=bias)
         self.w_output = nn.Linear(self.head_embed_dim * num_heads, embed_dim, bias=bias)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(
         self,
@@ -53,7 +54,12 @@ class MultiHeadAttention(nn.Module):
         v = self.w_value(value).view((*extra_dims_v, n_k, self.num_heads, self.head_embed_dim)).transpose(-2, -3) # [..., h, n_k, d_k]
 
         # Compute attention weights
-        attention_weights = self.compute_attention_weights(q, k, self.merge_mask(attention_mask, key_padding_mask), attention_head_mask)
+        attention_weights = self.compute_attention_weights(
+            q,
+            k,
+            self.merge_mask(attention_mask, key_padding_mask),
+            attention_head_mask)
+        attention_weights = self.dropout(attention_weights)
 
         attention = torch.matmul(attention_weights, v) # [..., h, n_q, d_k]
         attention = attention.transpose(-2, -3).reshape((*extra_dims_v, n_q, -1)) # [..., n_q, embed_dim]
@@ -143,3 +149,124 @@ class RelativeMultiHeadAttention(MultiHeadAttention):
         if attention_head_mask is not None:
             attention_weights = attention_weights * attention_head_mask.view((-1, 1, 1))
         return attention_weights
+
+"""
+TransformerEncoderBlock, MultiHeadAttentionBlock, SetAttentionBlock
+InducedSetAttentionBlock
+PoolingByMultiHeadAttention
+InducedSetEncoder
+"""
+
+class TransformerEncoderBlock(abc.ABC, nn.Module):
+    def __init__(
+        self,
+        mha: MultiHeadAttention,
+        feedforward_dim: int,
+        feedforward_activation: Optional[nn.Module] = nn.GELU(),
+        norm_first: bool = True,
+        dropout: float = 0.1,
+    ):
+        super().__init__()
+        self.mha = mha
+        self.feedforward_dim = feedforward_dim
+        self.feedforward_activation = feedforward_activation
+        self.norm_first = norm_first
+
+        self.norm1 = nn.LayerNorm(mha.embed_dim)
+        self.norm2 = nn.LayerNorm(mha.embed_dim)
+
+        self.feedforward = nn.Sequential(*(x for x in [
+                nn.Linear(mha.embed_dim, self.feedforward_dim),
+                self.feedforward_activation,
+                nn.Linear(self.feedforward_dim, mha.embed_dim),
+                nn.Dropout(dropout)
+            ] if x is not None
+        ))
+
+    def forward(
+        self,
+        src: torch.Tensor,
+        src_key_padding_mask: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        average_attention_weights: bool = True,
+        return_attention_weights: bool = False
+    ):
+        if self.norm_first:
+            src_norm = self.norm1(src)
+            attention_output, *extra_out = self.mha(
+                src_norm,
+                src_norm,
+                src_norm,
+                key_padding_mask=src_key_padding_mask,
+                attention_mask=attention_mask,
+                average_attention_weights=average_attention_weights,
+                return_attention_weights=return_attention_weights)
+            src = src + attention_output
+            output = src + self.feedforward(self.norm2(src))
+        else:
+            attention_output, *extra_out = self.mha(
+                src,
+                src,
+                src,
+                attention_mask=attention_mask,
+                key_padding_mask=src_key_padding_mask,
+                attention_head_mask=None,
+                average_attention_weights=average_attention_weights,
+                return_attention_weights=return_attention_weights)
+            src = self.norm1(src + attention_output)
+            output = self.norm2(src + self.feedforward(src))
+        if len(extra_out) > 0:
+            return output, *extra_out
+        return output
+
+
+class TransformerDecoderBlock(abc.ABC, nn.Module):
+    def __init__(
+        self,
+        mha: MultiHeadAttention,
+        norm_first: bool = True
+    ):
+        super().__init__()
+        self.mha = mha
+        self.norm_first = norm_first
+
+    @abc.abstractmethod
+    def forward(
+        self,
+        target: torch.Tensor,
+        memory: torch.Tensor
+    ):
+        return NotImplemented
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, encoder_layer: TransformerEncoderBlock, num_layers: int):
+        super().__init__()
+        self.layers = nn.ModuleList([copy.deepcopy(encoder_layer) for _ in range(num_layers)])
+
+    def forward(
+        self,
+        src: torch.Tensor,
+        src_key_padding_mask: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        average_attention_weights: bool = True,
+        return_attention_weights: bool = False
+    ):
+        attention_weights = [] if return_attention_weights else None
+        output = src
+        for layer in cast(Sequence[TransformerEncoderBlock], self.layers):
+            output, *extra_outputs = layer(
+                output,
+                src_key_padding_mask=src_key_padding_mask,
+                attention_mask=attention_mask,
+                average_attention_weights=average_attention_weights,
+                return_attention_weights=return_attention_weights)
+            extra_outputs = iter(extra_outputs)
+            if attention_weights is not None:
+                attention_weights.append(next(extra_outputs))
+        output = (output,)
+        if attention_weights is not None:
+            output += (torch.stack(attention_weights),)
+        if len(output) == 1:
+            return output[0]
+        return output
