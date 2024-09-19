9cf598dd2b4ce8f97d7a9ebe10e84a0566708cab
David Ludwig <davidludwigii@gmail.com>
2024-09-19 15:26:11 -0500

    Add SequenceTaxonomyDataset

--- src/dbtk/data/datasets.py
+++ src/dbtk/data/datasets.py
@@ -1,9 +1,30 @@
 from dnadb.fasta import FastaDb, FastaMappingDb
+from dnadb.taxonomy import TaxonomyDb
 from pathlib import Path
 from torch.utils.data import Dataset
 from typing import Callable, Optional, Sequence, Union
 
 from .interfaces import FastaFile
+
+class AmpliconSampleDataset(Dataset):
+    def __init__(
+        self,
+        samples: Sequence[FastaMappingDb],
+        transform: Optional[Callable] = None
+    ):
+        super().__init__()
+        self.samples = list(samples)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = self.samples[index]
+        if self.transform is not None:
+            x = self.transform(x)
+        return x
+
+    def __len__(self):
+        return len(self.samples)
+
 
 class SequenceDataset(Dataset):
     def __init__(self, sequences: Union[FastaFile, FastaDb, Path, str], transform: Optional[Callable] = None):
@@ -26,21 +47,32 @@ class SequenceDataset(Dataset):
         return entry
 
 
-class AmpliconSampleDataset(Dataset):
+class SequenceTaxonomyDataset(Dataset):
     def __init__(
         self,
-        samples: Sequence[FastaMappingDb],
-        transform: Optional[Callable] = None
+        sequences: Union[FastaDb, Path, str],
+        taxonomies: Union[TaxonomyDb, Path, str],
+        sequence_transform: Optional[Callable] = None,
+        taxonomy_transform: Optional[Callable] = None
     ):
         super().__init__()
-        self.samples = list(samples)
-        self.transform = transform
-
-    def __getitem__(self, index):
-        x = self.samples[index]
-        if self.transform is not None:
-            x = self.transform(x)
-        return x
+        if not isinstance(sequences, FastaDb):
+            sequences = FastaDb(sequences)
+        if not isinstance(taxonomies, TaxonomyDb):
+            taxonomies = TaxonomyDb(taxonomies)
+        self.sequences = sequences
+        self.taxonomies = taxonomies
+        self.sequence_transform = sequence_transform
+        self.taxonomy_transform = taxonomy_transform
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.sequences)
+
+    def __getitem__(self, index):
+        entry = self.sequences[index]
+        label = self.taxonomies[entry.identifier]
+        if self.sequence_transform is not None:
+            entry = self.sequence_transform(entry)
+        if self.taxonomy_transform is not None:
+            label = self.taxonomy_transform(label)
+        return entry, label
