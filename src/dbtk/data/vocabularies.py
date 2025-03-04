2170a2f53ffb5e4dcb99225dde5ee36df56edb14
David Ludwig <davidludwigii@gmail.com>
2025-03-04 22:40:38 +0000

    Update vocabulary to rename word to token

--- src/dbtk/data/vocabularies.py
+++ src/dbtk/data/vocabularies.py
@@ -4,29 +4,29 @@ def dna(kmer: int = 1):
     return list(map(str, map("".join, product("ACGT", repeat=kmer))))
 
 class Vocabulary:
-    def __init__(self, words):
-        self._word_to_index = {}
-        self._index_to_word = []
-        self.update(["[PAD]", "[UNK]", *words])
+    def __init__(self, tokens):
+        self._token_to_id = {}
+        self._id_to_token = []
+        self.update(["[PAD]", "[UNK]", *tokens])
 
-    def add(self, word):
-        if word in self._word_to_index:
+    def add(self, token):
+        if token in self._token_to_id:
             return
-        self._word_to_index[word] = len(self._index_to_word)
-        self._index_to_word.append(word)
+        self._token_to_id[token] = len(self._id_to_token)
+        self._id_to_token.append(token)
 
-    def update(self, words):
-        for word in words:
-            self.add(word)
+    def update(self, tokens):
+        for token in tokens:
+            self.add(token)
 
-    def __call__(self, words):
-        return map(self.__getitem__, words)
+    def __call__(self, tokens):
+        return map(self.__getitem__, tokens)
 
     def __getitem__(self, key):
-        return self._word_to_index.get(key, self._word_to_index["[UNK]"])
+        return self._token_to_id.get(key, self._token_to_id["[UNK]"])
 
     def __len__(self):
-        return len(self._index_to_word)
+        return len(self._id_to_token)
 
 
 class DnaVocabulary(Vocabulary):
